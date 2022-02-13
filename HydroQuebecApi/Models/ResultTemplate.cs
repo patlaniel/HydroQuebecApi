a057c4e79998f16ac008499d1545f7c58a426cbc
PatrickLaniel <patrick.laniel@cyclotouringquebec.com>
2022-02-13 16:27:10 -0500

    ResultWithComparison<T> is now equatable

--- HydroQuebecApi/Models/ResultTemplate.cs
+++ HydroQuebecApi/Models/ResultTemplate.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace HydroQuebecApi.Models
 {
-    public class ResultWithComparison<T>
+    public class ResultWithComparison<T> : IEquatable<ResultWithComparison<T>>
     {
         [JsonPropertyName("courant")] public T current { get; set; }
         public T compare { get; set; }
+        public bool Equals(ResultWithComparison<T> other) => GetHashCode() == other.GetHashCode();
+        public override bool Equals(object other) => Equals(other as ResultWithComparison<T>);
+        public override int GetHashCode() => current?.GetHashCode()??0;
+
     }
     public class ResultArrayTemplateWithComparison<T>
     {
